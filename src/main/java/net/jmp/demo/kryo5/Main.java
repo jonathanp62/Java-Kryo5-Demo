98a3d77a5d110adc934c90527590578afccd6b9e
Jonathan Parker <jonathanp62@gmail.com>
2024-05-15 13:06:53 -0400

    Added minimal code to get started

---
+++ src/main/java/net/jmp/demo/kryo5/Main.java
@@ -0,0 +1,67 @@
+package net.jmp.demo.kryo5;
+
+/*
+ * (#)Main.java 0.1.0   05/15/2024
+ *
+ * @author   Jonathan Parker
+ * @version  0.1.0
+ * @since    0.1.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.ext.XLogger;
+
+public final class Main {
+    /** The logger. */
+    private final XLogger logger = new XLogger(LoggerFactory.getLogger(this.getClass().getName()));
+
+    /**
+     * The default constructor.
+     */
+    private Main() {
+        super();
+    }
+
+    /**
+     * The execute method.
+     */
+    private void execute() {
+        this.logger.entry();
+
+        this.logger.info("Kryo5-Demo {}", Version.VERSION_STRING);
+
+        this.logger.exit();
+    }
+
+    /**
+     * The main method.
+     *
+     * @param   args    java.lang.String[]
+     */
+    public static void main(final String[] args) {
+        new Main().execute();
+    }
+}
